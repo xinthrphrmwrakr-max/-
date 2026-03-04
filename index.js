8565e8fb9133bf81b1038883c07c533b23c16cb2
xinthrphrmwrakr-max <xinthrphrmwrakr@gmail.com>
2026-03-04 15:36:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -74,22 +74,18 @@ res.sendStatus(200);
 // ================= MAIN =================
 async function handleEvent(event){
 
-if(event.type!=="message")return;
-if(event.message.type!=="text")return;
+if(event.type!=="message") return;
+if(event.message.type!=="text") return;
 
-const text=event.message.text.trim();
+const text = event.message.text.trim();
 
+if(event.source.type==="group" && text==="gid"){
+   const groupId = event.source.groupId;
 
-// ===== SHOW GROUP ID =====
-if(event.source.type === "group"){
-  const groupId = event.source.groupId;
-
-  console.log("GROUP ID =", groupId);
-
-  return client.replyMessage(event.replyToken,{
-    type:"text",
-    text:"GROUP ID : " + groupId
-  });
+   return client.replyMessage(event.replyToken,{
+      type:"text",
+      text:`GROUP ID : ${groupId}`
+   });
 }
 
 const profile=

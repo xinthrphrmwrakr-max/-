7485a587f6abe79d3eb85f9de6172c65bff9cc3e
xinthrphrmwrakr-max <xinthrphrmwrakr@gmail.com>
2026-03-04 19:49:07 +0700

    Update Fight.js

--- models/Fight.js
+++ models/Fight.js
@@ -1,25 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose=require("mongoose");
 
-const fightSchema = new mongoose.Schema({
-  teamA:String,
-  teamB:String,
-
-  status:{
-    type:String,
-    default:"open"
-  },
-
-  prices:[
-    {
-      priceNo:Number,
-      ต่อ:Number,
-      รอง:Number,
-      createdAt:{
-        type:Date,
-        default:Date.now
-      }
-    }
-  ]
+module.exports=mongoose.model("Fight",{
+fightId:Number,
+red:String,
+blue:String,
+rateRed:String,
+rateBlue:String,
+status:String
 });
-
-module.exports = mongoose.model("Fight",fightSchema);

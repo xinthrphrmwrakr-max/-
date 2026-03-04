6a97af86792049e561eb64b050d31ddabacc1037
xinthrphrmwrakr-max <xinthrphrmwrakr@gmail.com>
2026-03-04 17:39:28 +0700

    Update Table.js

--- models/Table.js
+++ models/Table.js
@@ -1,23 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose=require("mongoose");
 
-const tableSchema = new mongoose.Schema({
-  open: { type:Boolean, default:false },
-
-  rateRed:Number,
-  rateBlue:Number,
-
-  poolRed:{ type:Object, default:{} },
-  poolBlue:{ type:Object, default:{} },
-
-  bets:[
-    {
-      userId:String,
-      name:String,
-      side:String,
-      amount:Number
-    }
-  ]
-});
-
-module.exports =
-mongoose.model("Table",tableSchema);
+module.exports=mongoose.model(
+"Table",
+new mongoose.Schema({
+open:Boolean,
+rateRed:String,
+rateBlue:String,
+limit:Number,
+bets:Array
+})
+);

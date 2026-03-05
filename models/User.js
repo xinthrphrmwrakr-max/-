06fc168ba1a02112cd5b99515a58f15128527eb7
xinthrphrmwrakr-max <xinthrphrmwrakr@gmail.com>
2026-03-05 18:53:10 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose=require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema=new mongoose.Schema({
 userId:String,
 name:String,
-credit:{
-type:Number,
-default:0
-}
+credit:{type:Number,default:0}
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports=mongoose.model("User",UserSchema);
